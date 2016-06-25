11b25a02caed0d6ff2b23ed8deb64ae1c92f8731
santosneto <mn.neco@gmail.com>
2016-06-25 12:28:53 -0300

    Update 0.1

---
+++ R/diagtobit.R
@@ -0,0 +1,326 @@
+#'Local Influence Measures
+#'
+#'@description x
+#'
+#'@param model x
+#'@param tau x
+#'@param npoints x
+#'
+#'@return x
+#'
+#'@export
+
+diag.tobitn <-function(model,tau=0,npoints=0)
+{
+  X <- model.matrix(model)
+  p=ncol(X) #number parameters 
+  n=nrow(X) #number observations 
+  nu <- model$parms
+  y  <- as.numeric(model$y)[1:n]
+  c  <- (1*(y>tau))
+  muhat <- model$linear.predictors
+  sigmahat <- model$scale
+  delta <-(y-muhat)/sigmahat
+  phi         <- dnorm(delta)
+  Phi         <- pnorm(delta)
+  Wdelta      <- phi/Phi
+  deriv_phi   <- (-delta/sqrt(2*pi))*exp(-(delta^2)/2)
+  deriv_Wdelta<- (deriv_phi/(1-Phi))+((phi/(1-Phi))^2) #estou aqui!
+  
+  
+  #############################################################################################
+  ##########################################The Hessian Matrix#################################
+  #############################################################################################
+  
+  #Lsigmasigma
+  
+  Lsigmasigma  <- (1/(sigmahat^2))*(sum((1-c)*(2*Wdelta*delta)))+
+    (1/(sigmahat^2))*(sum((1-c)*delta*deriv_Wdelta*delta))+
+    (1/(sigmahat^2))*(sum(c*(1-3*(delta^2))))
+  
+  #Quantidades para obter Lbetabeta
+  
+  #C?lculo de V
+  
+  
+  parte1       <- (1-c)*(1/(sigmahat^2))*deriv_Wdelta	#Parte com censura
+  parte2       <- (1/(sigmahat^2))*c  #Parte sem censura
+  vi           <- parte1-parte2
+  v            <- as.vector(vi)
+  V            <- diag(v)
+  
+  #Lbetabeta
+  
+  
+  Lbeta        <- t(X)%*%V%*%X
+  
+  
+  #Quantidades para obter Lbetasigma
+  
+  #C?lculo de h
+  
+  parte11      <- (1-c)*((1/(sigmahat^2))*(Wdelta+delta*deriv_Wdelta)) #Parte com censura
+  parte12      <- -c*(2/(sigmahat^2))*delta  #Parte sem censura
+  hi           <- parte11+parte12
+  
+  #Lbetasigma
+  
+  Lbetasigma   <- t(X)%*%hi
+  
+  # Matriz de informa??o observada
+  
+  L1           <- cbind(Lbeta,Lbetasigma)
+  L2           <- cbind(t(Lbetasigma), Lsigmasigma)
+  
+  
+  Hessian <- rbind(L1,L2)
+  
+  #Obs.Infor <- Hessian
+  #Inv.Obs <- solve(-Obs.Infor)
+  #sqrt(diag(Inv.Obs))
+  # Inversa da matriz de informa??o observada
+  
+  invobservmatrix <- -solve(Hessian)
+  #diag(invobservmatrix)
+  
+  #Esquema de perturba??o pondera??o de casos
+  
+  
+  #C?lculo da matriz delta
+  Lsigma= Hessian[p+1,p+1]
+  Lbeta=Hessian[1:p,1:p]
+  b11=cbind(matrix(0, p, p), matrix(0, p, 1))
+  b12=cbind(matrix(0, 1, p), -Lsigma^(-1))
+  B1= rbind(b11, b12)  #parameter beta
+  b211 =cbind(-solve(Lbeta), matrix(0, p, 1))
+  b212= cbind(matrix(0, 1, p), matrix(0, 1, 1))
+  B2=rbind(b211,b212)  # parameter delta
+  
+  
+  #C?lculo da matriz delta
+  
+  
+  bi           <- -(1-c)*(1/sigmahat)*Wdelta+c*(1/sigmahat)*delta
+  b            <- as.vector(bi)								   
+  fi           <- -(1-c)*(1/sigmahat)*delta*Wdelta +c*(1/sigmahat)*(-1+delta^2)
+  deltabeta    <- t(X)%*%diag(b)
+  matrixdelta  <- rbind(deltabeta,t(fi))
+  F0            <- t(matrixdelta)%*%(invobservmatrix)%*%matrixdelta #matriz F
+  F1 <- t(matrixdelta)%*%(invobservmatrix - B1)%*%matrixdelta #matriz F beta
+  F2 <- t(matrixdelta)%*%(invobservmatrix - B2)%*%matrixdelta #matriz F sigma
+  
+  # C?lculo dos autovalores e autovetores da matriz F
+  
+  Lmax<-eigen(F0,symmetric=TRUE)$val[1]
+  dmax<-eigen(F0,symmetric=TRUE)$vec[,1]
+  Lmaxb<-eigen(F1,symmetric=TRUE)$val[1]
+  dmaxb<-eigen(F1,symmetric=TRUE)$vec[,1]
+  Lmaxs<-eigen(F2,symmetric=TRUE)$val[1]
+  dmaxs<-eigen(F2,symmetric=TRUE)$vec[,1]
+  # Influ?ncia Local Total
+  
+  
+  Ci<- 2*abs(diag(F0))
+  Ci<- Ci/sum(Ci)
+  
+  Cib<- 2*abs(diag(F1))
+  Cib<- Cib/sum(Cib)
+  
+  Cis<- 2*abs(diag(F2))
+  Cis<- Cis/sum(Cis)
+  #ponto de corte
+  corte1 <-2*mean(Ci)
+  corte2<-2*mean(Cib)
+  corte3<-2*mean(Cis)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(Ci, ylab="C",type="h", cex=0.5, ylim=c(0,0.40), pch=16,xlab="I")
+  abline(h=corte1,lty=2)
+  identify(Ci,n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(Cib, ylab="Cb",type="h", cex=0.5, ylim=c(0,0.40), pch=16,xlab="I")
+  abline(h=corte1,lty=2)
+  identify(Cib,n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(Cis, ylab="Cs",type="h", cex=0.5, ylim=c(0,0.40), pch=16,xlab="I")
+  abline(h=corte1,lty=2)
+  identify(Cis,n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(abs(dmax), ylab="L",type="h", ylim=c(0,1), cex=0.5, pch=16,xlab="I")
+  identify(abs(dmax),n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(abs(dmaxb), ylab="Lb",type="h", ylim=c(0,1), cex=0.5, pch=16,xlab="I")
+  identify(abs(dmaxb),n=npoints)
+  
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(abs(dmaxs), ylab="Ls",type="h", ylim=c(0,1), cex=0.5, pch=16,xlab="I")
+  identify(abs(dmaxs),n=npoints)
+  
+}
+
+#'Local Influence Measures
+#'
+#'@description x
+#'
+#'@param model x
+#'@param tau x
+#'@param npoints x
+#'
+#'@return x
+#'
+#'@export
+
+diag.tobitt <-function(model,tau=0,npoints=0)
+{
+  X <-model.matrix(model)
+  p=ncol(X) #number parameters 
+  n=nrow(X) #number observations 
+  nu <- model$parms
+  y  <- as.numeric(model$y)[1:n]
+  c  <- (1*(y>tau))
+  muhat <- model$linear.predictors
+  sigmahat <- model$scale
+  deltahat <-(y-muhat)/sigmahat
+  phi         <- dt(deltahat,model$parms)
+  Phi         <- pt(deltahat,model$parms)
+  Wdelta      <- phi/Phi
+  
+  
+  #############################################################################################
+  ##########################################The Hessian Matrix#################################
+  #############################################################################################
+  
+  const <- gamma((nu+1)/2)/((gamma(nu/2)*sqrt(pi*nu)))
+  deriv_phi <- -const*((nu+1)/nu)*deltahat*((1+((deltahat^2)/nu))^(-(nu+3)/2))
+  deriv_Wdelta <- (deriv_phi/Phi)+((phi/Phi)^2)
+  
+  ##############################################Lsigmasigma####################################
+  parte1 <- (1/(sigmahat^2))*sum((1-c)*(2*Wdelta*deltahat + (deltahat^2)*deriv_Wdelta))
+  parte2 <- (1/(sigmahat^2))*sum(c*(1 - (nu+1)*((3*(deltahat^2)*nu + (deltahat^4))/(((deltahat^2)+nu)^2)))) 
+  Lsigmasigma  <- (1/(sigmahat^2))*(sum((1-c)*(2*Wdelta*deltahat))) + (1/(sigmahat^2))*(sum((1-c)*deltahat*deriv_Wdelta*deltahat))+
+    (1/(sigmahat^2))*(sum(c*(1-3*(((nu+1)/nu)*(deltahat^2)/(1+((deltahat^2)/nu)))+
+                               2*((nu+1)/nu^2)*(deltahat^4)/(1+(deltahat^2)/nu)^2)))
+  
+  #################################################Lbetabeta ##################################
+  parte1       <- (1-c)*(1/(sigmahat^2))*deriv_Wdelta	#Parte com censura
+  parte2       <- c*(1/(sigmahat^2))*((((nu+1)/nu)/(1+((deltahat^2)/nu))) +
+                                        2*((nu+1)/nu^2)*(deltahat^2)/(1+(deltahat^2)/nu)^2) #Parte sem censura
+  vi           <- parte1 - parte2
+  v            <- as.vector(vi)
+  V            <- diag(v)
+  Lbeta        <- t(X)%*%V%*%X
+  
+  ######################################################Lbetasigma############################### 
+  parte11      <- (1-c)*((1/(sigmahat^2))*(Wdelta+deltahat*deriv_Wdelta)) #Parte com censura
+  parte12      <- c*(2/(sigmahat^2))*((-(nu+1)/nu)*(deltahat/(1+((deltahat^2)/nu))) 
+                                      +((nu+1)/nu^2)*(deltahat^3)/(1+(deltahat^2)/nu)^2)#Parte sem censura
+  hi           <- parte11 + parte12
+  Lbetasigma   <- t(X)%*%hi
+  
+  L1           <- cbind(Lbeta,Lbetasigma)
+  L2           <- cbind(t(Lbetasigma), Lsigmasigma)
+  
+  Hessian <- rbind(L1,L2)
+  
+  #Obs.Infor <- Hessian
+  #Inv.Obs <- solve(-Obs.Infor)
+  #sqrt(diag(Inv.Obs))
+  # Inversa da matriz de informa??o observada
+  
+  invobservmatrix <- -solve(Hessian)
+  #diag(invobservmatrix)
+  
+  #Esquema de perturba??o pondera??o de casos
+  Lsigma= Hessian[p+1,p+1]
+  Lbeta=Hessian[1:p,1:p]
+  b11=cbind(matrix(0, p, p), matrix(0, p, 1))
+  b12=cbind(matrix(0, 1, p), -Lsigma^(-1))
+  B1= rbind(b11, b12)  #parameter beta
+  b211 =cbind(-solve(Lbeta), matrix(0, p, 1))
+  b212= cbind(matrix(0, 1, p), matrix(0, 1, 1))
+  B2=rbind(b211,b212)  # parameter delta
+  
+  b311 =cbind(matrix(0, p, p), matrix(0, p, 1))
+  b312= cbind(matrix(0, 1, p), matrix(0, 1, 1))
+  B3=rbind(b311,b312)  # parameter theta
+  
+  #C?lculo da matriz delta
+  bi           <- -(1-c)*(1/sigmahat)*Wdelta+ c*(1/sigmahat)*((nu+1)/nu)*deltahat/(1+((deltahat^2)/nu))
+  b            <- as.vector(bi)								   
+  fi           <- -(1-c)*(1/sigmahat)*deltahat*Wdelta +c*(1/sigmahat)*(-1+((nu+1)/nu)*(deltahat^2)/(1+((deltahat^2)/nu)))
+  deltabeta    <- t(X)%*%diag(b)
+  matrixdelta  <- rbind(deltabeta,t(fi))
+  F0            <- t(matrixdelta)%*%(invobservmatrix)%*%matrixdelta #matriz F
+  F1 <- t(matrixdelta)%*%(invobservmatrix - B1)%*%matrixdelta #matriz F beta
+  F2 <- t(matrixdelta)%*%(invobservmatrix - B2)%*%matrixdelta #matriz F sigma
+  
+  # C?lculo dos autovalores e autovetores da matriz F
+  
+  Lmax<-eigen(F0,symmetric=TRUE)$val[1]
+  dmax<-eigen(F0,symmetric=TRUE)$vec[,1]
+  Lmaxb<-eigen(F1,symmetric=TRUE)$val[1]
+  dmaxb<-eigen(F1,symmetric=TRUE)$vec[,1]
+  Lmaxs<-eigen(F2,symmetric=TRUE)$val[1]
+  dmaxs<-eigen(F2,symmetric=TRUE)$vec[,1]
+  # Influ?ncia Local Total
+  
+  dev.new()
+  Ci<- 2*abs(diag(F0))
+  Ci<- Ci/sum(Ci)
+  
+  Cib<- 2*abs(diag(F1))
+  Cib<- Cib/sum(Cib)
+  
+  Cis<- 2*abs(diag(F2))
+  Cis<- Cis/sum(Cis)
+  #ponto de corte
+  corte1<-2*mean(Ci)
+  corte2<-2*mean(Cib)
+  corte3<-2*mean(Cis)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(Ci, ylab="C",type="h", cex=0.5, ylim=c(0,0.40), pch=16,xlab="I")
+  abline(h=corte1,lty=2)
+  identify(Ci,n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(Cib, ylab="Cb",type="h", cex=0.5, ylim=c(0,0.40), pch=16,xlab="I")
+  abline(h=corte1,lty=2)
+  identify(Cib,n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(Cis, ylab="Cs",type="h", cex=0.5, ylim=c(0,0.40), pch=16,xlab="I")
+  abline(h=corte1,lty=2)
+  identify(Cis,n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(abs(dmax), ylab="L",type="h", ylim=c(0,1), cex=0.5, pch=16,xlab="I")
+  identify(abs(dmax),n=npoints)
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(abs(dmaxb), ylab="Lb",type="h", ylim=c(0,1), cex=0.5, pch=16,xlab="I")
+  identify(abs(dmaxb),n=npoints)
+  
+  
+  dev.new()
+  par(mar=c(4.0,4.0,0.1,0.1))
+  plot(abs(dmaxs), ylab="Ls",type="h", ylim=c(0,1), cex=0.5, pch=16,xlab="I")
+  identify(abs(dmaxs),n=npoints)
+  
+}
---
+++ R/envelope.R
@@ -0,0 +1,217 @@
+#'Residuals
+#'
+#'@description x
+#'
+#'@param model x 
+#'@param type x
+#'@param tau x
+#'
+#'@return x
+#'
+#'@export
+
+residuals <- function(model,type,tau=0)
+{ # begin function
+  n <- summary(model)$n[1] 
+  y  <- as.numeric(model$y)[1:n]
+  c  <- (1*(y>tau))
+  nu <- model$parms
+  muhat = model$linear.predictors
+  sigmahat <- model$scale
+  deltahat <-(y-muhat)/sigmahat
+  S <- 1-pt(deltahat,nu)
+  rM <- c+log(S) # Martingale residual
+  rtM <- (sign(y-muhat)*(-2*(rM+c*log(c-rM)))^(1/2))#Martingale-type residual
+  rDC <- sign(y-muhat)*sqrt(-2*log(S))
+  
+  switch(type,
+         deviance = rDC,
+         martingale = rM,
+         martingalet = rtM
+  )
+  
+} #end function
+
+
+#'Envelope
+#'
+#'@description x
+#'
+#'@param model x
+#'@param res x
+#'@param nboot x
+#'@param alpha x
+#'@param intercept x
+#'@param td x
+#'
+#'@return
+#'
+#'@export
+
+envelope <- function(model,res="martingalet",nboot = 19,alpha=0.05,tau=0,intercept = "TRUE",td="t")
+{
+  n  <- summary(model)$n[1]
+  y  <- as.numeric(model$y)[1:n]
+  nu <- model$parms
+  muhat <- model$linear.predictors
+  sigmahat <- model$scale
+  deltahat <-(y-muhat)/sigmahat
+  X <- model.matrix(model)
+  var.explic <- X[,-1]
+  
+  rD <- residuals(model,res,tau=tau)#Martingale-type residual
+  
+  alpha1 <- ceiling(nboot*alpha)
+  alpha2 <- ceiling(nboot*(1-alpha))
+  e <- matrix(0,n,nboot)
+  
+  for(i in 1:nboot){
+    
+    ygerado  <- sigmahat*rt(n,nu) + muhat
+    n1  <- summary(model)$n[2] #n?mero de obs. cens.
+    pc       <- n1/n  #propor??o de obs. cens.
+    tau1      <- sort(ygerado)[pc*n]
+    yestrela <- ifelse(ygerado>tau1,ygerado,0)
+    
+    if(intercept == "FALSE") form = yestrela ~ var.explic - 1 else  form = yestrela ~ var.explic
+    
+    model1 = tobit(form,dist=td)
+    
+    rD2 <- residuals(model1,res,tau=tau)
+    e[,i] <- sort(rD2)
+  }
+  
+  e1<- numeric(n)
+  e2<- numeric(n)	
+  
+  for(j in 1:n){
+    
+    eo    <- sort(e[j,])
+    e1[j] <- eo[alpha1]
+    e2[j] <- eo[alpha2]
+  }
+  a<-  qqnorm(e1,plot.it=FALSE)$x
+  a1<-  qqnorm(e1,plot.it=FALSE)$y
+  b<-  qqnorm(e2,plot.it=FALSE)$x
+  b1<-  qqnorm(e2,plot.it=FALSE)$y
+  r<-  qqnorm(rD,plot.it=FALSE)$x
+  r1<-  qqnorm(rD,plot.it=FALSE)$y
+  
+  xx <- c(a,rev(b))
+  yy <- c(a1,rev(b1))
+  med   <- apply(e,1,mean)
+  faixa <- range(rD,e1,e2,med)
+  par(mar=c(4.0,4.0,0.1,0.1))   
+  plot(r,r1,type = "n", ylim=faixa,axes=FALSE,xlab="",ylab="")
+  par(new=T)
+  polygon(xx,yy,col="gray80",border=NA)
+  par(new=T)
+  qqnorm(rD,main="", ylim=faixa, ylab="R",xlab="Q", cex=0.7, pch=18)
+  par(new=T)
+  qqnorm(e1,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
+  par(new=T)
+  qqnorm(e2,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
+  par(new=T)
+  qqnorm(med,axes=F,type="l",main="",ylim=faixa,lty=2, xlab="", ylab="",col="black")
+  
+}
+
+#' Envelope
+#'
+#'@description x
+#'
+#' @param  model x
+#' @param nboot x
+#' @param alpha x
+#' @param tau x
+#' @param intercept x
+#'
+#'  
+#' @return 
+#' 
+#' @export
+#' 
+
+envelope.normal <- function(model,nboot = 19,alpha=0.05,tau=0,intercept = "TRUE")
+{
+  
+  n  <- summary(model)$n[1]
+  y  <- as.numeric(model$y)[1:n]
+  c <-  (1*(y>tau))
+  muhat <- model$linear.predictors
+  sigmahat <- model$scale
+  deltahat <-(y-muhat)/sigmahat
+  X <- model.matrix(model)
+  var.explic <- X[,-1]
+  
+  S <- 1-pnorm(deltahat)
+  rM <- c+log(S)
+  
+  # Res?duo componente do desvio Martingal 
+  
+  rD<- (sign((y-muhat))*(-2*(rM+c*log(c-rM)))^(1/2))
+  
+  
+  alpha1 <- ceiling(nboot*alpha)
+  alpha2 <- ceiling(nboot*(1-alpha))
+  e <- matrix(0,n,nboot)
+  
+  for(i in 1:nboot){
+    
+    ygerado  <- sigmahat*rnorm(n,0,1)+ muhat
+    n1  <- summary(model)$n[2] #n?mero de obs. cens.
+    pc       <- n1/n  #propor??o de obs. cens.
+    tau1      <- sort(ygerado)[pc*n]
+    yestrela <- ifelse(ygerado>tau1,ygerado,0)
+    status   <- (1*(ygerado>tau1))
+    
+    if(intercept == "FALSE") form = yestrela ~ var.explic - 1 else  form = yestrela ~ var.explic;
+    
+    model1 <- tobit(form)
+    muhat1 <- model1$linear.predictors
+    sigmahat1 <- model1$scale
+    deltahat1 <- (yestrela - muhat1)/sigmahat1
+    S1 <-1- pnorm(deltahat1)
+    rM1 <- status + log(S1)
+    rD2<- (sign((yestrela-muhat1))*(-2*(rM1+status*log(status-rM1)))^(1/2)) # Res?duo componente do desvio Martingal 
+    
+    e[,i]    <- sort(rD2)
+  }
+  
+  e1<- numeric(n)
+  e2<- numeric(n)	
+  
+  for(j in 1:n){
+    
+    eo    <- sort(e[j,])
+    e1[j] <- eo[alpha1]
+    e2[j] <- eo[alpha2]
+  }
+  
+  med   <- apply(e,1,mean)
+  faixa <- range(rD,e1,e2,med)
+  a<-  qqnorm(e1,plot.it=FALSE)$x
+  a1<-  qqnorm(e1,plot.it=FALSE)$y
+  b<-  qqnorm(e2,plot.it=FALSE)$x
+  b1<-  qqnorm(e2,plot.it=FALSE)$y
+  r<-  qqnorm(rD,plot.it=FALSE)$x
+  r1<-  qqnorm(rD,plot.it=FALSE)$y
+  
+  xx <- c(a,rev(b))
+  yy <- c(a1,rev(b1))
+  med   <- apply(e,1,mean)
+  faixa <- range(rD,e1,e2,med)
+  par(mar=c(4.0,4.0,0.1,0.1))   
+  plot(r,r1,type = "n", ylim=faixa,axes=FALSE,xlab="",ylab="")
+  par(new=T)
+  polygon(xx,yy,col="gray80",border=NA)
+  par(new=T)
+  qqnorm(rD,main="", ylim=faixa, ylab="R",
+         xlab="Q", cex=0.7, pch=18)
+  par(new=T)
+  qqnorm(e1,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
+  par(new=T)
+  qqnorm(e2,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
+  par(new=T)
+  qqnorm(med,axes=F,type="l",main="",ylim=faixa,lty=2, xlab="", ylab="",col="black")
+}
