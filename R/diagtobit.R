5a011289b7cc606384c779f4db1976a3a67c480e
santosneto <mn.neco@gmail.com>
2016-06-28 15:48:03 -0300

    update v0.3

--- R/diagtobit.R
+++ R/diagtobit.R
@@ -322,3 +322,152 @@ diag.tobitt <-function(model,tau=0,npoints=0)
   identify(abs(dmaxs),n=npoints)
   
 }
+
+
+#' Cook's distance
+#' 
+#' A measure that combines the information of leverage and residual of the observation
+#' 
+#' @param model
+#' @param tau
+#' @param npoints
+#' @param plot
+#' @param dist
+#' 
+#' @export
+
+cooks.distn <- function(model, tau, npoints, dist="t", plot=TRUE,ylim=NULL,xlim=NULL,pch=19,cex=0.5,type="p",col=NULL)
+  {
+  
+if(dist=="t")
+{
+  
+  
+} 
+  else{
+
+    theta <-c(model$coef,model$scale)
+    theta <-as.vector(theta)
+    c  <- (1*(y>0))
+    X    <-model.matrix(model) 
+    n    <-dim(X)[1]
+    p    <-dim(X)[2]
+    dados<-rbind(y,c,x1,x2,x3,x4,x5)
+    values <- matrix(0,n,1)
+    muhat <- model$linear.predictors
+    sigmahat <- model$scale
+    delta <- (y-muhat)/sigmahat
+    phi         <- dnorm(delta)
+    Phi         <- pnorm(delta)
+    Wdelta      <- phi/Phi
+    deriv_phi   <- (-delta/sqrt(2*pi))*exp(-(delta^2)/2)
+    deriv_Wdelta<- (deriv_phi/(1-Phi))+((phi/(1-Phi))^2) 
+    
+    #############################################################################################
+    ##########################################The Hessian Matrix#################################
+    #############################################################################################
+    #Lsigmasigma
+    Lsigmasigma  <- (1/(sigmahat^2))*(sum((1-c)*(2*Wdelta*delta)))+
+      (1/(sigmahat^2))*(sum((1-c)*delta*deriv_Wdelta*delta))+
+      (1/(sigmahat^2))*(sum(c*(1-3*(delta^2))))
+    #Quantidades para obter Lbetabeta
+    #C?lculo de V
+    parte1       <- (1-c)*(1/(sigmahat^2))*deriv_Wdelta	#Parte com censura
+    parte2       <- (1/(sigmahat^2))*c  #Parte sem censura
+    vi           <- parte1-parte2
+    v            <- as.vector(vi)
+    V            <- diag(v)
+    #Lbetabeta
+    Lbeta        <- t(X)%*%V%*%X
+    #Quantidades para obter Lbetasigma
+    #C?lculo de h
+    parte11      <- (1-c)*((1/(sigmahat^2))*(Wdelta+delta*deriv_Wdelta)) #Parte com censura
+    parte12      <- -c*(2/(sigmahat^2))*delta  #Parte sem censura
+    hi           <- parte11+parte12
+    #Lbetasigma
+    Lbetasigma   <- t(X)%*%hi
+    # Matriz de informa??o observada
+    L1           <- cbind(Lbeta,Lbetasigma)
+    L2           <- cbind(t(Lbetasigma), Lsigmasigma)
+    observmatrix <- rbind(L1, L2)
+    
+    for(i in 1:n)
+    {
+      dadosn<-subset(dados,select=-i)
+      
+      yi<-dadosn[1,]
+      v1i<-dadosn[3,]
+      v2i<-dadosn[4,]
+      v3i<-dadosn[5,]
+      v4i<-dadosn[6,]
+      v5i<-dadosn[7,]
+      form <- (yi ~ v1i+v2i+v3i+v4i+v5i) 
+      fit <- tobit(form)
+      
+      thetai=as.vector(c(fit$coef,fit$scale))
+      values[i,]=(1/(p+1))*t(theta-thetai)%*%(-observmatrix)%*%(theta-thetai)
+    }
+    
+    par(mar=c(4.0, 4.0,0.1,0.1))
+    plot(dist.cook, ylab="D",xlab="I",type="h",ylim=c(0,0.3))
+    identify(dist.cook,n=3)
+    
+    
+  }
+  
+
+par(mar=c(4.0,4.0,0.1,0.1)) #graphical parameter of the margin.
+plot(cook.dist, ylab=ylab,type=type, ylim=ylim, cex=cex, pch=pch,xlab=xlab,col=col,main = NULL,sub = NULL)
+identify(cook.dist,n=npoints)
+  
+}
+
+
+
+###################################################
+#information criterions
+####################################################
+#' Akaike's An Information Criterion
+#' 
+#' AICc is AIC with a correction for finite sample sizes.
+#' 
+#' @param object a fitted model object for which there exists a logLik method to extract the corresponding log-likelihood, or an object inheriting from class logLik
+#' @param object1 optionally more fitted model objects
+#' 
+#' @return 
+#' If just one object is provided, a numeric value with the corresponding AICc.
+#' 
+#' If two objects are provided, a data.frame with rows corresponding to the objects and columns 
+#' representing the number of 
+#' parameters in the model (df) and the AICc.
+#' 
+#' @export
+
+AICc <- function (object,object1=NULL) 
+{
+  if(is.null(object1))
+  {
+    aic <- AIC(object)
+    if (!is.numeric(aic)) 
+      stop("Cannot calculate AIC!")
+    k <- object$df
+    n <- length(object$y)
+    aicc <- aic + ((2 * k * (k + 1))/(n - k - 1))
+    return(aicc) 
+  } 
+  else{
+    aic <- AIC(object);aic1 <- AIC(object1)
+    if (!is.numeric(aic)&&!is.numeric(aic1)) 
+      stop("Cannot calculate AIC!")
+    k <- object$df ; k1 <- object$df
+    n <- length(object$y)
+    aicc <- aic + ((2 * k * (k + 1))/(n - k - 1))
+    aicc1 <- aic1 + ((2 * k1 * (k1 + 1))/(n - k1 - 1))
+    
+    out <- cbind(c(k,k1),c(aicc,aicc1))    
+    colnames(out) <- c("df","AICc")    
+    return(out[order(-out[2,],out[1,]),])
+  }
+}
+
+
