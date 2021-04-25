9d079cb0be4b63368bde3d05576956eae0a06408
santosneto <mn.neco@gmail.com>
2021-04-24 22:42:49 -0300

    update

--- R/envelope.R
+++ R/envelope.R
@@ -44,32 +44,35 @@
 #'
 #'@export
 
-residuals <- function(model,type,tau=0,dist ="t")
+residuals <- function(model, type, tau = 0, dist ="t")
 { # begin function
-  if(dist=="t"){
+  
+  if (summary(model)$n[2] != 0) cens <- 'left' else cens <- 'right'
+  
+  if (dist == "t") {
+    
     n <- summary(model)$n[1] 
     y  <- as.numeric(model$y)[1:n]
-    c  <- (1*(y>tau))
+    if (cens == 'left') c  <- (1*(y > tau)) else c  <- (1*(y <= tau)) 
     nu <- model$parms
     muhat = model$linear.predictors
     sigmahat <- model$scale
-    deltahat <-(y-muhat)/sigmahat
-    S <- 1-pt(deltahat,nu)
-    rM <- c+log(S) # Martingale residual
-    rtM <- (sign(y-muhat)*(-2*(rM+c*log(c-rM)))^(1/2))#Martingale-type residual
-    rDC <- sign(y-muhat)*sqrt(-2*log(S))
+    deltahat <- (y - muhat)/sigmahat
+    S <- 1 - pt(deltahat,nu)
+    rM <- c + log(S) # Martingale residual
+    rtM <- (sign(y - muhat)*(-2*(rM + c*log(c - rM)))^(1/2))#Martingale-type residual
+    rDC <- sign(y - muhat)*sqrt(-2*log(S))
   } else{
     n  <- summary(model)$n[1]
     y  <- as.numeric(model$y)[1:n]
-    c <-  (1*(y>tau))
+    if (cens == 'left') c  <- (1*(y > tau)) else c  <- (1*(y <= tau)) 
     muhat <- model$linear.predictors
     sigmahat <- model$scale
-    deltahat <-(y-muhat)/sigmahat
-    S <- 1-pnorm(deltahat)
-    rM <- c+log(S)
-    rtM <- (sign(y-muhat)*(-2*(rM+c*log(c-rM)))^(1/2))#Martingale-type residual
-    rDC <- sign(y-muhat)*sqrt(-2*log(S))
-    # Res?duo componente do desvio Martingal 
+    deltahat <- (y - muhat)/sigmahat
+    S <- 1 - pnorm(deltahat)
+    rM <- c + log(S)
+    rtM <- (sign(y - muhat)*(-2*(rM + c*log(c - rM)))^(1/2))#Martingale-type residual
+    rDC <- sign(y - muhat)*sqrt(-2*log(S))
   }
   switch(type,
          deviance = rDC,
@@ -85,7 +88,8 @@ residuals <- function(model,type,tau=0,dist ="t")
 #'@description Computes simulation envelopes of a normal tobit and tobit-t model.
 #'
 #'@usage
-#'function(model,res="martingalet",nboot = 19,alpha=0.05,tau=0,intercept = "TRUE",dist="t",col="gray80",xlab="Q",ylab="R")
+#'function(model,res="martingalet",nboot = 19,alpha=0.05,tau=0,
+#'intercept = "TRUE",dist="t",col="gray80",xlab="Q",ylab="R")
 #'
 #'@param model an object of class "tobit" as fitted by tobit. The formula should be a symbolic description of a regression model of type \code{y ~ x1 + x2 + ... + xp}.
 #'@param res character string indicating the type of residual 
@@ -119,112 +123,107 @@ residuals <- function(model,type,tau=0,dist ="t")
 #'
 #'#Analysis under the tobit-t model
 #'envelope(mt,dist = "t")
+#'@importFrom AER tobit
+#'@importFrom grDevices dev.new
+#'@importFrom graphics abline identify par polygon
+#'@importFrom stats AIC dnorm dt formula model.matrix pnorm pt qqnorm rnorm rt sd
 #'@export
 
-envelope <- function(model,res="martingalet",nboot = 19,alpha=0.05,tau=0,intercept = "TRUE",dist="t",col="gray80",xlab="Q",ylab="R")
+envelope <- function(model, res = "martingalet", nboot = 19, alpha = 0.05, tau = 0, intercept = "TRUE", dist = "t", col = "gray80", xlab = "Q", ylab = "R")
 {
-  if(dist == "t"){
+  
+  if (summary(model)$n[2] != 0) cens <- 'left' else cens <- 'right'
+  
+  
+  if (dist == "t") {
     n  <- summary(model)$n[1]
     y  <- as.numeric(model$y)[1:n]
     nu <- model$parms
     muhat <- model$linear.predictors
     sigmahat <- model$scale
-    deltahat <-(y-muhat)/sigmahat
+    deltahat <- (y - muhat)/sigmahat
     X <- model.matrix(model)
     var.explic <- X[,-1]
     
-    rD <- residuals(model,res,tau=tau,dist ="t")#Martingale-type residual
+    rD <- residuals(model, res, tau = tau, dist = "t")
     
     alpha1 <- ceiling(nboot*alpha)
-    alpha2 <- ceiling(nboot*(1-alpha))
+    alpha2 <- ceiling(nboot*(1 - alpha))
     e <- matrix(0,n,nboot)
     
-    for(i in 1:nboot){
+    for (i in 1:nboot) {
       
       ygerado  <- sigmahat*rt(n,nu) + muhat
-      n1  <- summary(model)$n[2] #n?mero de obs. cens.
-      pc       <- n1/n  #propor??o de obs. cens.
+      if (cens == 'left') n1  <- summary(model)$n[2]  else n1  <- summary(model)$n[4]
+      pc       <- n1/n  
       tau1      <- sort(ygerado)[pc*n]
-      yestrela <- ifelse(ygerado>tau1,ygerado,0)
-      
-      if(intercept == "FALSE") form = yestrela ~ var.explic - 1 else  form = yestrela ~ var.explic
-      
-      model1 = tobit(form,dist=dist)
-      
-      rD2 <- residuals(model1,res,tau=tau)
+      if (cens == 'left') yestrela <- ifelse(ygerado > tau1, ygerado,tau) else yestrela <- ifelse(ygerado <= tau1, ygerado,tau) 
+      if (intercept == "FALSE") form <- yestrela ~ var.explic - 1 else  form <- yestrela ~ var.explic
+      if (cens == 'left') model1 <- tobit(form, left = tau, dist = 't') else model1 <- tobit(form, right = tau, dist = 't')
+      rD2   <- residuals(model1, res, tau = tau)
       e[,i] <- sort(rD2)
     }
     
-    e1<- numeric(n)
-    e2<- numeric(n)	
+    e1 <- numeric(n)
+    e2 <- numeric(n)	
     
-    for(j in 1:n){
-      
+    for (j in 1:n) {
       eo    <- sort(e[j,])
       e1[j] <- eo[alpha1]
       e2[j] <- eo[alpha2]
     }
-    a<-  qqnorm(e1,plot.it=FALSE)$x
-    a1<-  qqnorm(e1,plot.it=FALSE)$y
-    b<-  qqnorm(e2,plot.it=FALSE)$x
-    b1<-  qqnorm(e2,plot.it=FALSE)$y
-    r<-  qqnorm(rD,plot.it=FALSE)$x
-    r1<-  qqnorm(rD,plot.it=FALSE)$y
     
-    xx <- c(a,rev(b))
-    yy <- c(a1,rev(b1))
+    a  <-  qqnorm(e1, plot.it = FALSE)$x
+    a1 <-  qqnorm(e1, plot.it = FALSE)$y
+    b  <-  qqnorm(e2, plot.it = FALSE)$x
+    b1 <-  qqnorm(e2, plot.it = FALSE)$y
+    r  <-  qqnorm(rD, plot.it = FALSE)$x
+    r1 <-  qqnorm(rD, plot.it = FALSE)$y
+    
+    xx    <- c(a,rev(b))
+    yy    <- c(a1,rev(b1))
     med   <- apply(e,1,mean)
     faixa <- range(rD,e1,e2,med)
-    par(mar=c(4.0,4.0,0.1,0.1))   
-    plot(r,r1,type = "n", ylim=faixa,axes=FALSE,xlab="",ylab="")
-    par(new=T)
-    polygon(xx,yy,col=col,border=NA)
-    par(new=T)
-    qqnorm(rD,main="", ylim=faixa, ylab=ylab,xlab=xlab, cex=0.7, pch=18)
-    par(new=T)
-    qqnorm(e1,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
-    par(new=T)
-    qqnorm(e2,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
-    par(new=T)
-    qqnorm(med,axes=F,type="l",main="",ylim=faixa,lty=2, xlab="", ylab="",col="black")
+    par(mar = c(4.0,4.0,0.1,0.1))   
+    plot(r,r1,type = "n", ylim = faixa,axes = FALSE, xlab = "", ylab = "")
+    par(new = T)
+    polygon(xx, yy, col = col, border = NA)
+    par(new = T)
+    qqnorm(rD, main = "", ylim = faixa, ylab = ylab,xlab = xlab, cex = 0.7, pch = 18)
+    par(new = T)
+    qqnorm(e1, axes = F,type = "l",main = "",ylim = faixa, lty = 1, xlab = "", ylab = "", col = "gray")
+    par(new = T)
+    qqnorm(e2, axes = F, type = "l", main = "",ylim = faixa, lty = 1, xlab = "", ylab = "", col = "gray")
+    par(new = T)
+    qqnorm(med, axes = F, type = "l", main = "", ylim = faixa, lty = 2, xlab = "", ylab = "", col = "black")
   } else{
     n  <- summary(model)$n[1]
     y  <- as.numeric(model$y)[1:n]
-    c <-  (1*(y>tau))
     muhat <- model$linear.predictors
     sigmahat <- model$scale
-    deltahat <-(y-muhat)/sigmahat
+    deltahat <- (y - muhat)/sigmahat
     X <- model.matrix(model)
     var.explic <- X[,-1]
-    rD <- residuals(model,res,tau=tau,dist ="normal")#Martingale-type residual
+    rD <- residuals(model,res,tau = tau, dist = "normal")#Martingale-type residual
     alpha1 <- ceiling(nboot*alpha)
-    alpha2 <- ceiling(nboot*(1-alpha))
+    alpha2 <- ceiling(nboot*(1 - alpha))
     e <- matrix(0,n,nboot)
     
-    for(i in 1:nboot){
-      
-      ygerado  <- sigmahat*rnorm(n,0,1)+ muhat
-      n1  <- summary(model)$n[2] #n?mero de obs. cens.
+    for (i in 1:nboot) {
+      ygerado  <- sigmahat*rnorm(n,0,1) + muhat
+      if (cens == 'left') n1  <- summary(model)$n[2]  else n1  <- summary(model)$n[4]
       pc       <- n1/n  #propor??o de obs. cens.
       tau1      <- sort(ygerado)[pc*n]
-      yestrela <- ifelse(ygerado>tau1,ygerado,0)
-      status   <- (1*(ygerado>tau1))
       
-      if(intercept == "FALSE") form = yestrela ~ var.explic - 1 else  form = yestrela ~ var.explic;
-      
-      model1 <- tobit(form)
-      muhat1 <- model1$linear.predictors
-      sigmahat1 <- model1$scale
-      deltahat1 <- (yestrela - muhat1)/sigmahat1
-      S1 <-1- pnorm(deltahat1)
-      rM1 <- status + log(S1)
-      rD2<- (sign((yestrela-muhat1))*(-2*(rM1+status*log(status-rM1)))^(1/2)) # Res?duo componente do desvio Martingal 
-      
-      e[,i]    <- sort(rD2)
+      if (cens == 'left') yestrela <- ifelse(ygerado > tau1, ygerado,tau) else yestrela <- ifelse(ygerado <= tau1, ygerado,tau) 
+      if (intercept == "FALSE") form <- yestrela ~ var.explic - 1 else  form <- yestrela ~ var.explic
+      if (cens == 'left') model1 <- tobit(form, left = tau) else model1 <- tobit(form, right = tau)
+       rD2   <- residuals(model1, res, tau = tau, dist = "normal")
+       e[,i]    <- sort(rD2)
     }
     
-    e1<- numeric(n)
-    e2<- numeric(n)	
+    e1 <- numeric(n)
+    e2 <- numeric(n)	
     
     for(j in 1:n){
       
@@ -235,30 +234,30 @@ envelope <- function(model,res="martingalet",nboot = 19,alpha=0.05,tau=0,interce
     
     med   <- apply(e,1,mean)
     faixa <- range(rD,e1,e2,med)
-    a<-  qqnorm(e1,plot.it=FALSE)$x
-    a1<-  qqnorm(e1,plot.it=FALSE)$y
-    b<-  qqnorm(e2,plot.it=FALSE)$x
-    b1<-  qqnorm(e2,plot.it=FALSE)$y
-    r<-  qqnorm(rD,plot.it=FALSE)$x
-    r1<-  qqnorm(rD,plot.it=FALSE)$y
+    a     <-  qqnorm(e1,plot.it = FALSE)$x
+    a1    <-  qqnorm(e1,plot.it = FALSE)$y
+    b     <-  qqnorm(e2,plot.it = FALSE)$x
+    b1    <-  qqnorm(e2,plot.it = FALSE)$y
+    r     <-  qqnorm(rD,plot.it = FALSE)$x
+    r1    <-  qqnorm(rD,plot.it = FALSE)$y
     
     xx <- c(a,rev(b))
     yy <- c(a1,rev(b1))
     med   <- apply(e,1,mean)
     faixa <- range(rD,e1,e2,med)
-    par(mar=c(4.0,4.0,0.1,0.1))   
-    plot(r,r1,type = "n", ylim=faixa,axes=FALSE,xlab="",ylab="")
-    par(new=T)
-    polygon(xx,yy,col="gray80",border=NA)
-    par(new=T)
-    qqnorm(rD,main="", ylim=faixa, ylab=ylab,
-           xlab=xlab, cex=0.7, pch=18)
-    par(new=T)
-    qqnorm(e1,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
-    par(new=T)
-    qqnorm(e2,axes=F,type="l",main="",ylim=faixa,lty=1, xlab="", ylab="",col="gray")
-    par(new=T)
-    qqnorm(med,axes=F,type="l",main="",ylim=faixa,lty=2, xlab="", ylab="",col="black") 
+    par(mar = c(4.0,4.0,0.1,0.1))   
+    plot(r,r1,type = "n", ylim = faixa, axes = FALSE, xlab = "", ylab = "")
+    par(new = T)
+    polygon(xx,yy,col = "gray80",border = NA)
+    par(new = T)
+    qqnorm(rD,main = "", ylim = faixa, ylab = ylab,
+           xlab = xlab, cex = 0.7, pch = 18)
+    par(new = T)
+    qqnorm(e1,axes = F,type = "l",main = "",ylim = faixa,lty = 1, xlab = "", ylab = "", col = "gray")
+    par(new = T)
+    qqnorm(e2, axes = F,type = "l", main = "", ylim = faixa, lty = 1, xlab = "", ylab = "", col = "gray")
+    par(new = T)
+    qqnorm(med, axes = F, type = "l", main = "", ylim = faixa, lty = 2, xlab = "", ylab = "", col = "black") 
     
     
   }
